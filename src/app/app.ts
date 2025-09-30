e501843c57e5ddfb13672f0043d2a42805836109
Lawal Olawale <lawalolawale32@gmail.com>
2025-09-30 12:20:54 +0100

    feat(dashboard): created dashboard component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('stats');
-}
+export class App {}
