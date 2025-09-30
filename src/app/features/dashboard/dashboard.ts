c187800de20fc367bc6201f849523e4f3e103c9e
Lawal Olawale <lawalolawale32@gmail.com>
2025-09-30 13:05:09 +0100

    feat: setup and configured spartan

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.css'],
+  imports: [HlmButtonImports],
 })
 export class DashboardComponent {
   // Add component logic here
