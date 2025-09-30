d0d9953fed38f2d3f7e2a12056ccccc8e92d5399
Lawal Olawale <lawalolawale32@gmail.com>
2025-09-30 03:30:14 +0100

    feat: setup up the project with tailwind

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,9 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('stats');
