cc604ae74fb37aa137706d9543a8ea352c26ba69
Lawal Olawale <lawalolawale32@gmail.com>
2025-09-30 13:23:11 +0100

    fix: fix inter font issue

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
